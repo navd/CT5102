827c681d28e67c53623ef4f1988eb2c2f851a7d6
Jim Duggan <jim.duggan@nuigalway.ie>
2015-09-29 09:24:10 +0100

    Update for lecture 4

--- 03 Matrices and Data Frames/03 Census.R
+++ 03 Matrices and Data Frames/03 Census.R
@@ -3,6 +3,7 @@ library(reshape)
 library(ggplot2)
 
 c <- read.xls("03 Matrices and Data Frames/CensusData.xlsx")
+c1 <- read.xls("03 Matrices and Data Frames/Assignment.xlsx",sheet=2)
 
 
 c$AnnualTotal<-apply(c[,2:5],1,sum)
